a0c2997b0a8db0310ef35779697aea60bb35867f
Doug Simpson <douglas.simpson@spglobal.com>
2020-07-10 21:59:14 -0400

    init

---
+++ Bonject.Campaign.API/Bonject.Campaign.API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bonject.Campaign.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        public string Index()
+        {
+            return "Campaign";
+        }
+    }
+}
